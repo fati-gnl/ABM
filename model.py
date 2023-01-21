babd0d968e8eeedfa98c726d5713d0a885c845c8
Karolina Drabent <karolina.drabent@tcl.com>
2023-01-21 15:59:02 +0100

    Remove grid

--- model.py
+++ model.py
@@ -18,18 +18,15 @@ class CopCitizen(Model):
 
         super().__init__()
 
-        self.height = height
-        self.width = width
         self.initial_citizens = initial_citizens
         self.initial_cops = initial_cops
-        self.sheep_reproduction_chance = sheep_reproduction_chance
-        self.wolf_death_chance = wolf_death_chance
 
         #Add a schedule for citizens and cops seperately to prevent race-conditions
+        # Check if they can be drawn instead (citizens)
         self.schedule_Citizen = RandomActivation(self)
         self.schedule_Cop = RandomActivation(self)
 
-        self.grid = MultiGrid(self.width, self.height, torus=True)
+        # self.grid = MultiGrid(self.width, self.height, torus=True)
 
         # Data collector to be able to save the data - cop and citizen count
         # Todo: We should collect other data - I guess total money
@@ -39,24 +36,12 @@ class CopCitizen(Model):
 
         # Create citizens
         for i in range(self.initial_citizens):
-            x = random.randrange(self.grid.width)
-            y = random.randrange(self.grid.height)
-            citizen = Citizen(self.next_id(), self, (x,y))
-            self.grid.place_agent(citizen, (x, y))
+            citizen = Citizen(self.next_id(), self)
             self.schedule_Citizen.add(citizen)
 
         # Create cops: No two cops should be placed on the same cell
-        # Todo: No two cops should be next to each other
-        current_coord = []
         for i in range(self.initial_cops):
-            x = random.randrange(self.grid.width)
-            y = random.randrange(self.grid.height)
-            while (x, y) in current_coord:
-                x = random.randrange(self.grid.width)
-                y = random.randrange(self.grid.height)
-            current_coord.append((x, y))
-            cop = Cop(self.next_id(), self, (x,y))
-            self.grid.place_agent(cop, (x, y))
+            cop = Cop(self.next_id(), self)
             self.schedule_Cop.add(cop)
 
         # Needed for the Batch run
