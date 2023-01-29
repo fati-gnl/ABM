6c391079ff5fcae0bac35dce496e1a2802201ecf
s2356546 <f.gonzalez-novolopez@student.utwente.nl>
2023-01-29 15:29:36 +0100

    Added Functions class

--- agents.py
+++ agents.py
@@ -17,127 +17,137 @@ class Functions:
         action = id_act[np.argmax(np.random.multinomial(1, distribution))]
         return action
 
-class PayoffAgent(Agent):
-    def __init__(self, unique_id, model, lambda_):
+
+class Citizen(Agent, Functions):
+    def __init__(self,
+                 unique_id,
+                 model,
+                 lambda_,
+                 prob_success_complain,
+                 complain_reward,
+                 fine_amount,
+                 memory_size,
+                 cost_accept_mean,
+                 cost_accept_std,
+                 cost_silence_mean,
+                 cost_silence_std):
         super().__init__(unique_id, model)
+
+        self.action = None
+        self.cost_complain = self.model.cost_complain
+        self.cost_accept = np.random.normal(loc=cost_accept_mean, scale=cost_accept_std)
+        self.cost_silence = np.random.normal(loc=cost_silence_mean, scale=cost_silence_std)
+        self.prob_success_complain = prob_success_complain
+        self.complain_reward = complain_reward
         self.lambda_ = lambda_
+        self.fine_amount = fine_amount
 
-        self.actions = []
+        self.memory_size = memory_size
+        self.memory = [0] * self.memory_size
 
-        self.action = None
+        self.id_act = {0: "accept_complain",
+                       1: "accept_silent",
+                       2: "reject_complain",
+                       3: "reject_silent"}
 
-    def init_action_dicts(self):
-        # Payoff matrix for the individual citizen.
-        self.payoffs = dict.fromkeys(self.actions, 0)
-        self.action_count = dict.fromkeys(self.actions, 0)
+    def get_action(self, bribe_amount):
+        # utility_accept_complain = -self.bribe_amount - self.cost_complain - self.prob_success_complain*(0.5*self.complain_reward) - self.cost_accept
+        # utility_accept_reject = -self.bribe_amount - self.cost_silence - self.cost_accept
+        # utility_reject_complain = -self.fine_amount - self.cost_complain - self.prob_success_complain*self.complain_reward
+        # utility_reject_reject = -self.fine_amount - self.cost_silence
 
-    def avg_payoffs(self):
-        '''
-        Take an average of payoffs
-        :return: averaged payoff matrix
-        '''
-        mean_payoffs = [
-            self.payoffs[action] / self.action_count[action] if self.action_count[action] > 0 else self.payoffs[action]
-            for action in self.actions]
-        return mean_payoffs
+        utilities = np.array([10,
+                              10,
+                              5,
+                              5])
 
-    def softmax(self, x):
-        """Compute softmax values for each sets of scores in x."""
-        e_x = np.exp(x - np.max(x))
-        return e_x / e_x.sum(axis=0)
+        self.action = self.sample_action(utilities, self.id_act, self.lambda_)
 
-    def choose_action(self):
-        '''
-        :return: Action number
-        '''
-        avg_payoffs = np.array(self.avg_payoffs())
-        # logit equilibrium distribution
-        distribution = self.softmax(self.lambda_ * avg_payoffs)
-        return numpy.argmax(numpy.random.multinomial(1, distribution))
+        return self.action
 
-
-class Citizen(PayoffAgent):
-    def __init__(self, unique_id, model, lambda_, reward = 50):
-        super().__init__(unique_id, model, lambda_)
-
-        self.actions = Actions.get_actions(Citizen)
-        self.init_action_dicts()
-        self.action = None
-
-        self.reward = reward
-
-    def step(self):
-        '''
-        This method should check first if a citizen is in the caught citizens. Then if so the citizen should play
-        '''
-        self.action = None
-        if self.unique_id in [citizen.unique_id for citizen in self.model.caught_citizens]:
-            self.play()
-
-    def play(self):
-        '''
-        If Cititzen selected they play(meets cop).
-        The logic of the meeting is implemented here and not in the cop class.
-        '''
-        # all the moves are done here and not in the cop step function
-        # First pick a cop that is assigned to this citizen
-        cop = self.model.get_cop()
-        # cop makes an action based on payoff matrix
-        cop.do_action()
-
-        # depending on the choices different payoff is assigned
-        if cop.action == "bribe":
-            payoff_matrix = {}
-
-            payoff_matrix["accept_and_complain"] = -cop.bribe_amount - self.model.cost_of_complaining - self.model.prob_succesful_complain * \
-                                             (self.model.reward_citizen - self.model.penalty_citizen) - self.model.cost_of_accepting
-            payoff_matrix["accept_and_silent"] = -cop.bribe_amount - self.model.cost_of_silence - self.model.cost_of_accepting
-            payoff_matrix["reject_and_complain"] = -cop.fine - self.model.cost_of_complaining - self.model.prob_succesful_complain * self.reward
-            payoff_matrix["reject_and_silent"] = -cop.fine - self.model.cost_of_silence
-
-            payoffs = payoff_matrix.values()
-            payoff_probabilities = np.argmax(self.softmax(payoffs))
-
-        else:
-            pass
-
-            #self.payoffs[self.action] += 0
-            #cop.payoffs[cop.action] += cop.moral_commitment
-
-    def do_action(self):
-        '''
-        This function sets an action to a chosen action.
-        The action is chosen based on the payoff matrix of the agent.
-        '''
-        self.action = Actions.get_actions(Citizen)[self.choose_action()]
-        self.action_count[self.action] += 1
-
-
-class Cop(PayoffAgent):
-
-    def __init__(self, unique_id, model, lambda_, bribe_mean_std=(0.5, 0.1), moral_commitment_mean_std=(0.3, 0.2)):
-        super().__init__(unique_id, model, lambda_)
-        self.actions = Actions.get_actions(Cop)
-        self.action = None
-
-        self.jail_time = random.randint(0,1)
-        self.bribe_amount = bribe_amount
-        self.fine = fine
-
-        # Each cop has a different moral commitment value, drawn from a normal distribution.
-        self.moral_commitment = np.random.normal(loc=moral_commitment_mean_std[0], scale=moral_commitment_mean_std[1])
-        # Each cop has a different bribe value as they're greediness varies, drawn from a normal distribution.
-        self.bribe = np.random.normal(loc=bribe_mean_std[0], scale=bribe_mean_std[1])
-
-        self.init_action_dicts()
+    def approximate_prob_accept(self):
+        return sum(self.memory) / self.memory_size
 
     def step(self):
         self.action = None
 
-    def do_action(self):
-        '''
-        This function sets an action to a chosen action.
-        The action is chosen based on the payoff matrix of the agent.
-        '''
-        self.action = Actions.get_actions(Cop)[self.choose_action()]
-        self.action_count[self.action] += 1
+    def update_memory(self, update):
+        self.memory.pop(0)
+        self.memory.append(update)
+
+
+class Cop(Agent, Functions):
+    def __init__(self, unique_id, model, in_jail, lambda_, memory_size, bribe_amount_mean, bribe_amount_std):
+        super().__init__(unique_id, model)
+
+        self.in_jail = in_jail
+        self.lambda_ = lambda_
+        self.memory_size = memory_size
+        self.memory = [0] * self.memory_size
+        self.action = None
+        self.bribe_amount_mean = bribe_amount_mean
+        self.bribe_amount_std = bribe_amount_std
+
+        self.id_act = {0: "bribe", 1: "not_bribe"}
+
+    def step(self):
+        self.action = None
+        # Check whether this cop can play this round
+        play = self.validate_play()
+
+        # If cop can play, chose a random citizen from the avialable citizens and sample an action for the cop
+        if play:
+            cop_action = self.get_action()
+            citizen = self.model.get_citizen()
+
+            # If the cop bribes, sample an action for the citizen
+            if cop_action == "bribe":
+                cit_action = citizen.get_action(self.bribe_amount)
+
+                # If the citizen complains, give the cop a jail time based on the ground truth of the probability of getting caught
+                if "complain" in cit_action:
+                    self.in_jail = self.model.jail_time * \
+                                   np.random.multinomial(1, [self.model.prob_caught, 1 - self.model.prob_caught])[0]
+
+                # If the citizen accepts to bribe, update this in the memory for the cop
+                if "accept" in cit_action:
+                    self.update_memory(1)
+
+                # If the citizen rejects to bribe, update this in the memory for the cop
+                if "reject" in cit_action:
+                    self.update_memory(0)
+
+        # If the cop cannot play, check whether the cop is in jail and if the cop is in jail, reduce their sentence by 1
+        elif self.in_jail > 0:
+            self.in_jail -= 1
+
+    def validate_play(self):
+        play = True if self in self.model.cops_playing else False
+        return play
+
+    def get_action(self):
+        self.bribe_amount = np.random.normal(loc=self.bribe_amount_mean, scale=self.bribe_amount_std)
+
+        approx_prob_caught = self.approximate_prob_caught()
+        approx_prob_accept = self.approximate_prob_accept()
+
+        utility_bribe = (1 - approx_prob_caught) * (approx_prob_accept * self.bribe_amount)
+        utility_not_bribe = approx_prob_caught * self.model.jail_time
+
+        utilities = np.array([utility_bribe, utility_not_bribe])
+
+        self.action = self.sample_action(utilities, self.id_act, self.lambda_)
+
+        return self.action
+
+    def approximate_prob_accept(self):
+        return sum(self.memory) / self.memory_size
+
+    def approximate_prob_caught(self):
+        team = self.model.id_team[self.unique_id]
+        m = self.model.team_jailed[team]
+        return m / self.model.team_size
+
+    def update_memory(self, update):
+        self.memory.pop(0)
+        self.memory.append(update)
